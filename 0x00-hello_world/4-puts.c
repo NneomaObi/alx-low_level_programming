4aeecd5f1ef23730c35f80790d40079df9e0af40
NneomaObi <obidegwu.nneoma@yahoo.com>
2022-12-08 15:00:01 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /* Main print line of code */
 /* Main functions that prints text */
 /*Return = 0 */
-int main(void) 
+int main(void)
 {puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
