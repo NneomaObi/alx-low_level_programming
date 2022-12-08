faa12342f887182d8e755f0adcedf1f806985d06
NneomaObi <obidegwu.nneoma@yahoo.com>
2022-12-08 15:03:30 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
-/* Main print line of code */
-/* Main functions that prints text */
-/*Return = 0 */
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * 
+ *
+ *    * Description: 'the program's description'
+ *
+ *     * 
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *       */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
