182db4b571f1e59316cd0326724f1aac1e26ddb9
NneomaObi <obidegwu.nneoma@yahoo.com>
2022-12-10 07:05:46 -0800

    Three number combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,15 +15,15 @@ int main(void)
 	{
 		for (j = i + 1; j < 9; j++)
 		{
-			for(k = j+1; k < 10; k++)
+			for (k = j + 1; k < 10; k++)
 			{
-				if(i != j && i != k && j != k)
+				if (i != j && i != k && j != k)
 				{
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
-					
-					if(i + j + k < 27)
+
+					if (i + j + k < 24)
 					{
 						putchar(',');
 						putchar(' ');
@@ -35,7 +35,6 @@ int main(void)
 	putchar('\n');
 	return (0);
 
-			
 }
 
 
