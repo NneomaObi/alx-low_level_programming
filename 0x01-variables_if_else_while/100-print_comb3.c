f46470616d36f87bf100fd92891320273481a9d8
NneomaObi <obidegwu.nneoma@yahoo.com>
2022-12-10 06:32:30 -0800

    Combination of 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i < '8'; i++)
+	{	for (j = i + 1; j < '9'; j++)
+		{
+			if (i != j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+
+				if(i + j < 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	return (0);
+
+
+}
