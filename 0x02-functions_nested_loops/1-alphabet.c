d8287c84a4063bd3764a7213572250e4b2d9d823
NneomaObi <obidegwu.nneoma@yahoo.com>
2022-12-13 06:30:05 -0800

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* main - check the code
+*
+* Return - Always 0
+*/
+
+int main(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+		_putchar(letters);
+		_putchar('\n');
+
+	return (0);
+}
