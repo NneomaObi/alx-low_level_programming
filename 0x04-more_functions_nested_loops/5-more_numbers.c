73a9c67df4de8a1f3c8f25896faa4b5e8d797683
NneomaObi <obidegwu.nneoma@yahoo.com>
2022-12-15 13:35:54 -0800

    Functions prints number 1-14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,13 @@ void more_numbers(void)
 {
 	int i, x;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (x = 0; x <= 14; x++)
 		{
 			if (x > 10)
 				_putchar(x / 10 + '0');
+
 			_putchar(x % 10 + '0');
 		}
 		_putchar('\n');
