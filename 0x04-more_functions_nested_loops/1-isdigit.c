aef6167274e680efdda93f54ebd34a9eed800d91
NneomaObi <obidegwu.nneoma@yahoo.com>
2022-12-15 09:18:36 -0800

    Function that checks for digits

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 * _isdigit - A function that checks for a digit (0 through 9).
 * @c: An input integer
@@ -17,5 +18,6 @@ int _isdigit(int c)
 			break;
 		}
 	}
+
 	return (checkdigit);
 }
