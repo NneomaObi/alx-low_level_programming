62a8bc4c39fa2f17d54b1d7142a2a6da0522eae2
NneomaObi <obidegwu.nneoma@yahoo.com>
2022-12-15 18:13:44 -0800

    Function prints remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,7 +9,6 @@
 * @year: year
 * Return: void
 */
-
 void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0 || (year % 400 == 0 && !(year % 100 == 0)))
@@ -18,7 +17,7 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++;
 		}
-		
+
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
@@ -27,9 +26,8 @@ void print_remaining_days(int month, int day, int year)
 		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-
 		}
-		else 
+		else
 		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
