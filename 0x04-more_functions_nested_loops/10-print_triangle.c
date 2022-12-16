46bcdd9028a4e9fe2e2aba972a48a89cfad35d70
NneomaObi <obidegwu.nneoma@yahoo.com>
2022-12-16 05:47:40 -0800

    Function prints triangle size

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,13 +8,13 @@
 void print_triangle(int size)
 {
 	int x, j, n = size -1;
-
+	
 	if (size > 0)
 	{
 		for (x = 0; x < size; x++)
 		{
 			for (j = 0; j < size; j++)
-			{	
+			{
 				if (j < n)
 					_putchar(' ');
 				else
